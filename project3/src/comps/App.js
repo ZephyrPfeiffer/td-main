a399b733008d4c22406fd33262b501d5d0e2e396
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2023-05-17 12:57:05 -0400

    create layout/styling for footer component

--- project3/src/comps/App.js
+++ project3/src/comps/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Nav from './Nav';
 import MainContainer from './MainContainer';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <MainContainer />
+      <Footer />
     </div>
   );
 }
---
+++ project3/src/comps/Footer.js
@@ -0,0 +1,18 @@
+import './Footer.css'
+
+function Footer() {
+
+  return (
+
+    <footer class="footer-container">
+
+      <span>Start Date: </span>
+      <span>End Date: </span>
+
+    </footer>
+
+  )
+
+}
+
+export default Footer;
